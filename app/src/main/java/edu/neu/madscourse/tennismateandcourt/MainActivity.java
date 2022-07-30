0ece8de17f6ce31bcae8dc341263489b6ac316aa
eddieyan95 <yufanyan95@gmail.com>
2022-07-30 16:20:49 -0400

    Update Navigation Fragment, Dock is functional

--- app/src/main/java/edu/neu/madscourse/tennismateandcourt/MainActivity.java
+++ app/src/main/java/edu/neu/madscourse/tennismateandcourt/MainActivity.java
@@ -3,12 +3,15 @@ package edu.neu.madscourse.tennismateandcourt;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.concurrent.CountedCompleter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
 
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigation);
-//        NavController navController = Navigation.findNavController(this,R.id.fragment);
-//        AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
-//        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
-//        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//        NavController navController= Navigation.findNavController(this,R.id.fragmentContainerView);
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
     }
 }
