0fb4c34a4630145207b1e5f8155c1ce247507c2c
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-08-01 23:57:18 -0700

    can display map & 1 marker

--- app/src/main/java/edu/neu/madscourse/tennismateandcourt/MapFragment.java
+++ app/src/main/java/edu/neu/madscourse/tennismateandcourt/MapFragment.java
@@ -1,5 +1,18 @@
 package edu.neu.madscourse.tennismateandcourt;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -12,9 +25,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
-
+public class MapFragment extends Fragment{
     private MapViewModel mViewModel;
+    GoogleMap map;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -23,8 +36,22 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                map = googleMap;
+                LatLng NEUBoston = new LatLng(42.33962289703249, -71.08975307364507);
+                map.addMarker(new MarkerOptions().position(NEUBoston).title("NEU Boston Tennis Court"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(NEUBoston));
+            }
+        });
+        return rootView;
+
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -33,4 +60,22 @@ public class MapFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+//    GoogleMap map;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_map);
+//
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap){
+//        map = googleMap;
+//        LatLng NEUBoston = new LatLng(42.33962289703249, -71.08975307364507);
+//        map.addMarker(new MarkerOptions().position(NEUBoston).title("NEU Boston"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(NEUBoston));
+//    }
 }
